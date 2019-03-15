6061dacec5ef4767994a5bbb4352d770410ecc26
visualstudio <visualstudio@122-PC31>
2019-03-15 19:02:00 -0400

    final commit

--- c0723560_c0732132/Program.cs
+++ c0723560_c0732132/Program.cs
@@ -82,6 +82,7 @@ namespace c0723560_c0732132
         {
             BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
 
+
             ba.Credit(5.77);
             ba.Debit(11.22);
             Console.WriteLine("Current balance is ${0}", ba.Balance);
