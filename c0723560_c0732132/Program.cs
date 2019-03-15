5c6b30606f221f464d56667f06830fcec6c326ba
visualstudio <visualstudio@122-PC31>
2019-03-15 18:10:37 -0400

    commit-1

--- c0723560_c0732132/Program.cs
+++ c0723560_c0732132/Program.cs
@@ -8,13 +8,54 @@ using System.Threading.Tasks;
  * Dheeraj Jain c0723560
  */
 namespace c0723560_c0732132
-{
-    class Program
 
-    {
-        static void Main(string[] args)
+{ 
+        /// Bank Account demo class.
+ 
+        public class BankAccount
         {
+            private string m_customerName;
 
+            private double m_balance;
+
+            private bool m_frozen = false;
+
+            private BankAccount()
+            {
+            }
+            public BankAccount(string customerName, double balance)
+            {
+                m_customerName = customerName;
+                m_balance = balance;
+            }
+            public string CustomerName
+            {
+                get { return m_customerName; }
+            }
+            public double Balance
+            {
+                get { return m_balance; }
+            }
+            public void Debit(double amount)
+            {
+                if (m_frozen)
+                {
+                    throw new Exception("Account frozen");
+                }
+
+                if (amount > m_balance)
+                {
+                    throw new ArgumentOutOfRangeException("amount");
+                }
+
+                if (amount < 0)
+                {
+                    throw new ArgumentOutOfRangeException("amount");
+                }
+
+                m_balance += amount; // intentionally incorrect code
+            }
+
+           
         }
     }
-}
